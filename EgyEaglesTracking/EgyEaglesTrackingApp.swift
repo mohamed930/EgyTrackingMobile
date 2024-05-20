b52eaf564c0899c432138c7aedb0dbc85f58089f
mohamed930 <mohamed@gmail.com>
2024-05-20 19:49:33 +0300

    Initial Commit

---
+++ EgyEaglesTracking/EgyEaglesTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EgyEaglesTrackingApp.swift
+//  EgyEaglesTracking
+//
+//  Created by Mohamed Ali on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct EgyEaglesTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
