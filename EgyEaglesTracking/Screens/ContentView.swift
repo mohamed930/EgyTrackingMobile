eed0b049a404cc605a2604a19b77cbdb43d03654
mohamed930 <mohamed@gmail.com>
2024-05-20 20:33:35 +0300

    Finish the login screen commit

---
+++ EgyEaglesTracking/Screens/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  EgyEaglesTracking
+//
+//  Created by Mohamed Ali on 20/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var email: String = ""
+    
+    var body: some View {
+        VStack {
+            Image("EgyEagles")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150)
+                .padding([.top], 96)
+                .padding([.bottom],50)
+            
+            ZStack {
+                HStack {
+                    Image("Email")
+                    
+                    Spacer()
+                }
+                .padding([.horizontal],30)
+                .offset(y: -4)
+                
+                TextField("Email",text: self.$email)
+                    .frame(height: 46)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding([.horizontal], 56)
+                    .cornerRadius(23)
+                    .overlay(RoundedRectangle(cornerRadius: 23).stroke(Color("#B1B1B1")))
+                    .padding([.horizontal], 10)
+                    .padding([.bottom], 8)
+                    .font(.system(size: 16,weight: .medium))
+            }
+            
+            
+            ZStack {
+                HStack {
+                    Image("password")
+                    
+                    Spacer()
+                }
+                .padding([.horizontal],30)
+                .offset(y: 0)
+                
+                SecureField("Password",text: self.$email)
+                    .frame(height: 46)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding([.horizontal], 56)
+                    .cornerRadius(23)
+                    .overlay(RoundedRectangle(cornerRadius: 23).stroke(Color("#B1B1B1")))
+                    .padding([.horizontal], 10)
+                    .font(.system(size: 16,weight: .medium))
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
